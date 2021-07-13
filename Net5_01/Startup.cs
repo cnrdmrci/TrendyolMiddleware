79defa72e5367e8485d231cd416aa722d231835a
caner.demirci <caner.demirci@trendyol.com>
2021-07-13 18:55:59 +0300

    change configuration

--- Net5_01/Startup.cs
+++ Net5_01/Startup.cs
@@ -1,23 +1,13 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using Trendyol.TyMiddleware.Configuration;
+using Net5_01.NewTest;
 using Trendyol.TyMiddleware.Extensions;
-using Trendyol.TyMiddleware.Middlewares;
-using Trendyol.TyMiddleware.Outputs.Logging.Concrete;
-using Trendyol.TyMiddleware.Outputs.Logging.LogConfig;
 
-namespace SampleApi
+namespace Net5_01
 {
     public class Startup
     {
@@ -32,8 +22,12 @@ namespace SampleApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-            services.AddTrendyolMiddleware();
-            services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new OpenApiInfo {Title = "SampleApi", Version = "v1"}); });
+            services.AddScoped<ITestService, TestService>();
+            services.AddTyMiddleware(config =>
+            {
+                config.AddMiddlewareProfileType(typeof(LogConfigProfile));
+            });
+            services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new OpenApiInfo {Title = "Net5_01", Version = "v1"}); });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,20 +37,10 @@ namespace SampleApi
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "SampleApi v1"));
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Net5_01 v1"));
             }
-
-            app.UseTyMiddleware(configuration =>
-                configuration.AddMiddleware(
-                    new LogMiddleware(
-                        new LogConfiguration()
-                        {
-                            StatusCodeLogEnabled = true,
-                            RequestBodyLogEnabled = true,
-                            ResponseBodyLogEnabled = true,
-                            ControllerLogEnabled= true,
-                            LogType = LogType.Console
-                        })));
+            
+            app.UseTyMiddleware();
 
             app.UseHttpsRedirection();
 
