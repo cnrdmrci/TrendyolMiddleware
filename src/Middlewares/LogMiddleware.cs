66063a66a8f3f9868517dd286a27a9b8a6609408
caner.demirci <caner.demirci@trendyol.com>
2021-07-16 18:14:26 +0300

    refactor for filter

--- src/Middlewares/LogMiddleware.cs
+++ src/Middlewares/LogMiddleware.cs
@@ -1,23 +1,16 @@
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
-using Trendyol.TyMiddleware.BaseMiddleware;
-using Trendyol.TyMiddleware.Configuration;
-using Trendyol.TyMiddleware.Model;
-using Trendyol.TyMiddleware.Outputs.Logging.Abstract;
-using Trendyol.TyMiddleware.Outputs.Logging.LogHelper;
-using Trendyol.TyMiddleware.Profile;
-using TrendyolMiddleware.Utils;
+using Trendyol.TyMiddleware.Core.MiddlewareCoreProvider;
+using Trendyol.TyMiddleware.Services.LogServices.LogBaseService;
 
 namespace Trendyol.TyMiddleware.Middlewares
 {
     public class LogMiddleware : IBaseMiddleware
     {
-        private readonly LogMiddlewareProfile _logProfile;
+        private readonly ILogService _logService;
 
-        public LogMiddleware()
+        public LogMiddleware(ILogService logService)
         {
-            _logProfile = BaseConfiguration.GetBaseProfiles().OfType<LogMiddlewareProfile>().FirstOrDefault();
+            _logService = logService;
         }
 
         public Task RequestHandler(BaseMiddlewareModel baseMiddlewareModel)
@@ -27,45 +20,9 @@ namespace Trendyol.TyMiddleware.Middlewares
 
         public Task ResponseHandler(BaseMiddlewareModel baseMiddlewareModel)
         {
-            ApiFilter apiFilter = GetApiFilter(baseMiddlewareModel);
-            if (apiFilter != null)
-            {
-                LogFactory logFactory = LogTypeSelector.GetLogMethod(_logProfile);
-                logFactory.Log(baseMiddlewareModel,apiFilter);
-            }
-            
+            _logService.Log(baseMiddlewareModel);
+
             return Task.CompletedTask;
-        }
-
-        private ApiFilter GetApiFilter(BaseMiddlewareModel baseMiddlewareModel)
-        {
-            ApiFilterModel apiFilterModel = _logProfile.ApiFilterModel;
-            List<ApiFilter> apiFilters = apiFilterModel.ApiFilters;
-            
-            if (!apiFilters.IsNullOrEmpty() && !string.IsNullOrWhiteSpace(baseMiddlewareModel.Controller))
-            {
-                apiFilters = apiFilters.Where(x => x.Controller == baseMiddlewareModel.Controller).ToList();
-            }
-            if(!apiFilters.IsNullOrEmpty() && !string.IsNullOrWhiteSpace(baseMiddlewareModel.Action))
-            {
-                apiFilters = apiFilters.Where(x => x.Action == baseMiddlewareModel.Action).ToList();
-            }
-            if(!apiFilters.IsNullOrEmpty() && !string.IsNullOrWhiteSpace(baseMiddlewareModel.HttpMethod))
-            {
-                apiFilters = apiFilters.Where(x => x.Method == baseMiddlewareModel.HttpMethod).ToList();
-            }
-            
-            if (apiFilterModel.ApiFilterType == ApiFilterType.Exclude && apiFilters.Any())
-            {
-                return null;
-            }
-            
-            if (apiFilterModel.ApiFilterType == ApiFilterType.Include)
-            {
-                return apiFilters.FirstOrDefault();
-            }
-
-            return new ApiFilter();
         }
     }
 }
